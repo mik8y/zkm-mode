4c47a937715bac93a04e0ec67d19af352b895dd9
Mik8y <mik8y@gearhead.local>
2009-12-17 10:23:37 +0900

    make function zkm-mode-syntax-table

--- zkm-mode.el
+++ zkm-mode.el
@@ -41,15 +41,30 @@ For detail, see `comment-dwim'."
       '(("classpath[\s\t\n]\\|open[\s\t\n]\\|trimExclude[\s\t\n]\\|trimUnexclude[\s\t\n]\\|resetTrimExclusions[\s\t\n]\\|trim[\s\t\n]\\|exclude[\s\t\n]\\|unexclude[\s\t\n]\\|resetExclusions[\s\t\n]\\|obfuscateFlowExclude[\s\t\n]\\|obfuscateFlowUnexclude[\s\t\n]\\|resetObfuscateFlowExclusions[\s\t\n]\\|stringEncryptionExclude[\s\t\n]\\|stringEncryptionUnexclude[\s\t\n]\\|resetStringEncryptionExclusions[\s\t\n]\\|existingSerializedClasses[\s\t\n]\\|resetExistingSerializedClasses[\s\t\n]\\|fixedClasses[\s\t\n]\\|resetFixedClasses[\s\t\n]\\|groupings[\s\t\n]\\|resetGroupings[\s\t\n]\\|obfuscate[\s\t\n]\\|unobfuscate[\s\t\n]\\|saveAll[\s\t\n]\\|saveAllOld[\s\t\n]\\|gc[\s\t\n]\\|execute[\s\t\n]\\|print[\s\t\n]" . font-lock-function-name-face)
         ("and[\s\t\n]" . font-lock-constant-face)))
 
+
+;;; SYNTAX TABLE
+(defvar zkm-mode-syntax-table
+  (let ((table (make-syntax-table)))
+    (modify-syntax-entry ?\/ ". 124b" table)
+    (modify-syntax-entry ?* ". 23" table)
+    (modify-syntax-entry ?\n "> b" table)
+    table)
+  "Syntax table used in ZKM mode.")
+
 ;; define the major mode.
-(define-derived-mode zkm-mode fundamental-mode
-  "zkm-mode is a major mode for editing language zkm."
+(defun zkm-mode ()
+  "Major mode for editing ZKM script."
+  (interactive)
+  (kill-all-local-variables)
+  
+  (set-syntax-table zkm-mode-syntax-table)
   (setq font-lock-defaults '(zkm-keywords))
   
   ;; modify the keymap
   (define-key zkm-mode-map [remap comment-dwim] 'zkm-comment-dwim)
   
-  (modify-syntax-entry ?\/ ". 124b" zkm-mode-syntax-table)
-  (modify-syntax-entry ?* ". 23" zkm-mode-syntax-table)
-  (modify-syntax-entry ?\n "> b")
   )
+
+(provide 'zkm-mode)
+
+;;; zkm-mode.el ends here
