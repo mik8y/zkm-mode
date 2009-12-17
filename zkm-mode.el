040e12278489b0732f3972f3fb3b0356261814e7
Mik8y <mik8y@gearhead.local>
2009-12-17 14:13:11 +0900

    add screenshot of zkm-mode

--- zkm-mode.el
+++ zkm-mode.el
@@ -36,10 +36,78 @@ For detail, see `comment-dwim'."
   (let ((deactivate-mark nil) (comment-start "//") (comment-end ""))
     (comment-dwim arg)))
 
+(defvar zkm-font-lock-keywords
+  (list
+   (list zkm-comment-pattern)))
 ;; keywords for syntax coloring
 (setq zkm-keywords
       '(("classpath[\s\t\n]\\|open[\s\t\n]\\|trimExclude[\s\t\n]\\|trimUnexclude[\s\t\n]\\|resetTrimExclusions[\s\t\n]\\|trim[\s\t\n]\\|exclude[\s\t\n]\\|unexclude[\s\t\n]\\|resetExclusions[\s\t\n]\\|obfuscateFlowExclude[\s\t\n]\\|obfuscateFlowUnexclude[\s\t\n]\\|resetObfuscateFlowExclusions[\s\t\n]\\|stringEncryptionExclude[\s\t\n]\\|stringEncryptionUnexclude[\s\t\n]\\|resetStringEncryptionExclusions[\s\t\n]\\|existingSerializedClasses[\s\t\n]\\|resetExistingSerializedClasses[\s\t\n]\\|fixedClasses[\s\t\n]\\|resetFixedClasses[\s\t\n]\\|groupings[\s\t\n]\\|resetGroupings[\s\t\n]\\|obfuscate[\s\t\n]\\|unobfuscate[\s\t\n]\\|saveAll[\s\t\n]\\|saveAllOld[\s\t\n]\\|gc[\s\t\n]\\|execute[\s\t\n]\\|print[\s\t\n]" . font-lock-function-name-face)
         ("and[\s\t\n]" . font-lock-constant-face)))
+
+(defun zkm-comment-dwim ()
+  "Comment or uncomment the current line or text selection."
+  (interactive)
+  
+  ;; If there's no text selection, comment or uncomment the line
+  ;; depending whether the WHOLE line is a comment. If there is a text
+  ;; selection, using the first line to determine whether to
+  ;; comment/uncomment.
+  (let (p1 p2)
+    (if (and transient-mark-mode mark-active)
+        (save-excursion
+          (setq p1 (region-beginning) p2 (region-end))
+          (goto-char p1)
+          (if (wholeLineIsCmt-p)
+              (zkm-uncomment-region p1 p2)
+            (zkm-comment-region p1 p2)
+            ))
+      (progn
+        (if (wholeLineIsCmt-p)
+            (zkm-uncomment-current-line)
+          (zkm-comment-current-line)
+          )) )))
+
+(defun wholeLineIsCmt-p ()
+  (save-excursion
+    (move-beginning-of-line 1)
+    (looking-at "[\s\t]*//")
+    ))
+
+(defun my-comment-current-line ()
+  (interactive)
+  (move-beginning-of-line 1)
+  (insert "//")
+  )
+
+(defun my-uncomment-current-line ()
+  "Remove “//” (if any) in the beginning of current line."
+  (interactive)
+  (when (wholeLineIsCmt-p)
+    (move-beginning-of-line 1)
+    (search-forward "//")
+    (delete-backward-char 2)
+    ))
+
+(defun my-comment-region (p1 p2)
+  "Add “//” to the beginning of each line of selected text."
+  (interactive "r")
+  (let ((deactivate-mark nil))
+    (save-excursion
+      (goto-char p2)
+      (while (>= (point) p1)
+        (zkm-comment-current-line)
+        (previous-line)
+        ))))
+
+(defun my-uncomment-region (p1 p2)
+  "Remove “//” (if any) in the beginning of each line of selected text."
+  (interactive "r")
+  (let ((deactivate-mark nil))
+    (save-excursion
+      (goto-char p2)
+      (while (>= (point) p1)
+        (zkm-uncomment-current-line)
+        (previous-line) )) ))
 
 
 ;;; SYNTAX TABLE
@@ -61,9 +129,7 @@ For detail, see `comment-dwim'."
   (setq font-lock-defaults '(zkm-keywords))
   
   ;; modify the keymap
-  (define-key zkm-mode-map [remap comment-dwim] 'zkm-comment-dwim)
-  
-  )
+  (define-key zkm-mode-map [remap comment-dwim] 'zkm-comment-dwim))
 
 (provide 'zkm-mode)
 
